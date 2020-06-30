275940d340b35240daeef91290b6b99de7fbee9f
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-06-30 20:09:51 +0900

    時間の実装OK、しかしインプットがnullでも投稿されてしまっている問題発見

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,18 +1,30 @@
 $(function(){
   function buildMessages(message){
+    
     var attachedImage = ""
-    if (message.image !== null) {
+    if (message.imageJSN !== null) {
       attachedImage = `<div class="MessageBox>
                         ${message.imageJSN}
                       </div>`
     }
+    
+    function getToday() {
+      var t = new Date();
+      var year = t.getFullYear();
+      var mth = t.getMonth() + 1;
+      var date = t.getDate();
+      var hour = t.getHours();
+      var mins = t.getMinutes();
+      return year + "年" + mth + "月" + date + "日 " + hour + "時" + mins + "分";
+    }
+
     let html = `<div class="MessageBox">
                   <div class="MessageInfo">
                     <div class="MessageInfo__userName">
                       ${message.userJSN}
                     </div>
                     <div class="MessageInfo__date">
-                      ${message.timeJSN}
+                      ${getToday()}
                     </div>
                   </div>
                   <div class="Message">
