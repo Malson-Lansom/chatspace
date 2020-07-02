b59ff443e2fc25dc59532d53f0917bcc73fb0cc2
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-07-02 12:40:41 +0900

    自動更新機能の実装

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,8 +1,11 @@
-$(function(){
+$(function() {
+
+  setInterval(reloadMessages, 7000);
+
   function buildMessages(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -21,7 +24,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -37,40 +40,33 @@ $(function(){
         </div>
       </div>`
       return html;
-
     }
-    
-
-
-
   }
 
-  $('.Content').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this)
-    let url = $(this).attr('action')
+  let reloadMessages = function() {
+    let last_message_id = $('.MessageBox:last').data("message-id");
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      //リクエストしたいのは/groups/id番号/api/messages
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(message){
-      let html = buildMessages(message)
-      $('.MessageField').append(html)
-      // $('.Content__inputContent').val('')
-      $('form')[0].reset();
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
-      //投稿が実行された後、ボタンが非活性になる状態を取り除く？
-      $('.Content__Submit').prop("disabled", false)
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertMessagesHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertMessagesHTML += buildMessages(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.MessageField').append(insertMessagesHTML);
+        $('.MessageField').animate({scrollTop: $('.MessageField')[0].scrollHeight});
+      }
     })
-    .fail(function(){
-      alert("メッセージ送信エラーです")
-    })
-  })
-
-
+    .fail(function() {
+      alert('error');
+    });
+  };
 
 });
